bc864ac98c2912b153a82617c8c5db4708fe8fb7
Paulo Machado <paulomachadof3@gmail.com>
2025-12-30 20:00:26 -0300

    Update posts.tsx

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -1,32 +1,33 @@
 import Link from 'next/link'
-import { formatDate, getBlogPosts } from 'app/blog/utils'
+import { formatDate, getProjects } from 'app/projects/utils'
 
-export function BlogPosts() {
-  let allBlogs = getBlogPosts()
+export function Projects() {
+  const allProjects = getProjects()
 
   return (
     <div>
-      {allBlogs
+      {allProjects
         .sort((a, b) => {
           if (
-            new Date(a.metadata.publishedAt) > new Date(b.metadata.publishedAt)
+            new Date(a.metadata.publishedAt) >
+            new Date(b.metadata.publishedAt)
           ) {
             return -1
           }
           return 1
         })
-        .map((post) => (
+        .map((project) => (
           <Link
-            key={post.slug}
+            key={project.slug}
             className="flex flex-col space-y-1 mb-4"
-            href={`/blog/${post.slug}`}
+            href={`/projects/${project.slug}`}
           >
             <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
               <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums">
-                {formatDate(post.metadata.publishedAt, false)}
+                {formatDate(project.metadata.publishedAt, false)}
               </p>
               <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
-                {post.metadata.title}
+                {project.metadata.title}
               </p>
             </div>
           </Link>
