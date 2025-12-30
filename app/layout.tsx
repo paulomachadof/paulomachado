a16e741c6ca41b91d1bf251f903345926aee8a46
Paulo Machado <paulomachadof3@gmail.com>
2025-12-30 20:42:42 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,3 +64,15 @@ export default function RootLayout({
     </html>
   )
 }
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    return [
+      { source: '/projects', destination: '/blog' },
+      { source: '/projects/:slug', destination: '/blog/:slug' },
+    ]
+  },
+}
+
+module.exports = nextConfig
