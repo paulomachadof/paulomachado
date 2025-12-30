2851783b05902b17e3b9d00b8aa7bec05cebf967
Paulo Machado <paulomachadof3@gmail.com>
2025-12-30 20:35:17 -0300

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,17 +1,17 @@
-import { getBlogPosts } from 'app/blog/utils'
+import { getProjects } from 'app/blog/utils'
 
 export const baseUrl = 'https://portfolio-blog-starter.vercel.app'
 
 export default async function sitemap() {
-  let blogs = getBlogPosts().map((post) => ({
+  const projects = getProjects().map((post) => ({
     url: `${baseUrl}/blog/${post.slug}`,
     lastModified: post.metadata.publishedAt,
   }))
 
-  let routes = ['', '/resume', '/blog'].map((route) => ({
+  const routes = ['', '/resume', '/blog'].map((route) => ({
     url: `${baseUrl}${route}`,
     lastModified: new Date().toISOString().split('T')[0],
   }))
 
-  return [...routes, ...blogs]
+  return [...routes, ...projects]
 }
