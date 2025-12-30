e9f9e3dcf89757fb21e2e56572ab2ec53669f572
Paulo Machado <paulomachadof3@gmail.com>
2025-12-30 20:32:52 -0300

    Update posts.tsx

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -7,20 +7,16 @@ export function Projects() {
   return (
     <div>
       {allProjects
-        .sort((a, b) => {
-          if (
-            new Date(a.metadata.publishedAt) >
-            new Date(b.metadata.publishedAt)
-          ) {
-            return -1
-          }
-          return 1
-        })
+        .sort(
+          (a, b) =>
+            new Date(b.metadata.publishedAt).getTime() -
+            new Date(a.metadata.publishedAt).getTime()
+        )
         .map((project) => (
           <Link
             key={project.slug}
             className="flex flex-col space-y-1 mb-4"
-            href={`/app/${blog.slug}`}
+            href={`/blog/${project.slug}`}
           >
             <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
               <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums">
