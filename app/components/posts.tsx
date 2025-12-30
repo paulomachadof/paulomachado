6b6face8b4975a77b30e26be85e01f17c7b0fe9f
Paulo Machado <paulomachadof3@gmail.com>
2025-12-30 20:30:37 -0300

    Update posts.tsx

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -20,7 +20,7 @@ export function Projects() {
           <Link
             key={project.slug}
             className="flex flex-col space-y-1 mb-4"
-            href={`/projects/${project.slug}`}
+            href={`/app/${blog.slug}`}
           >
             <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
               <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums">
